1319b29894472bcc93020486c93364b4747f047b
Mike Taslakjian <mike.taslakjian.techx@gmail.com>
2025-11-21 11:19:17 +0400

    Added .gitignore file to not commit node modules

--- task3.ts
+++ task3.ts
@@ -20,9 +20,3 @@ interface User {
 
 type userSummary = Pick<User, "id" | "name">;
 
-// 3. Build a Small TypeScript Project with Multiple Modules:
-// Step 1: Create a math.ts module with some basic math functions (e.g., add, subtract).
-// Step 2: Create a utils.ts module with a utility function (e.g., capitalize, logMessage).
-// Step 3: Create a main.ts file where you import and use the functions from math.ts and utils.ts.
-
-// Hint: Make sure to organize your functions properly and use appropriate module syntax with export and import.
